468d12d9841a2b5c5df067960d4ab54a39cb78aa
tenzu <huangxin.cliff@gmail.com>
2018-03-23 13:20:13 +0800

    with aggregates

--- 3D_fibres/LOCALS.py
+++ 3D_fibres/LOCALS.py
@@ -23,7 +23,13 @@ sph_ctr = np.zeros((f_num * n_sph, 3))  # centers of spheres
 rd = np.linspace(0, 2 * math.pi, n + 1)  # angles of sphere centers
 local_cs = []  # local cordinate systems for fibres
 b1_num = 5  # number of aggregate type 1
-b1_ctr = np.zeros((b1_num, 4))  # aggregate A info (x, y, z, radius)
+b1_ctr = np.zeros((b1_num, 4))  # aggregate 1 info (x, y, z, radius)
+b2_num = 12  # number of aggregate type 2
+b2_ctr = np.zeros((b2_num, 4))  # aggregate 2 info (x, y, z, radius)
+b3_num = 32  # number of aggregate type 3
+b3_ctr = np.zeros((b3_num, 4))  # aggregate 3 info (x, y, z, radius)
+b4_num = 64  # number of aggregate type 4
+b4_ctr = np.zeros((b4_num, 4))  # aggregate 4 info (x, y, z, radius)
 
 
 # generate a random local cordinate system
@@ -104,32 +110,155 @@ def b1_ctrs():
         y_b1 = random.uniform(-math.sqrt(x_ ** 2 - x_b1 ** 2), math.sqrt(x_ ** 2 - x_b1 ** 2))
         z_ = 2 * r_disk * td_ratio - 2 * r_b1 - 2 * min_gap
         z_b1 = random.uniform(-z_ / 2, z_ / 2)
-        for i in range(0, int(f_num * (n * p + 1))):
-            if (sph_ctr[i, 0] - x_b1) ** 2 + (sph_ctr[i, 1] - y_b1) ** 2 > (r_sph + r_b1) ** 2 + min_gap:
+        for i in range(0, int(f_num * (n * p + 1)),1):
+            if (sph_ctr[i, 0] - x_b1) ** 2 + (sph_ctr[i, 1] - y_b1) ** 2 +(sph_ctr[i, 2] - z_b1) ** 2 > (r_sph + r_b1) ** 2 + min_gap:
                 b1_avlb = b1_avlb * 1
             else:
                 b1_avlb = b1_avlb * 0
-        for i in range(0, b1_cont - 1):
-            if (b1_ctr[i, 0] - x_b1) ** 2 + (b1_ctr[i, 1] - y_b1) ** 2 > (b1_ctr[i, 2] + r_b1) ** 2 + min_gap:
+        for i in range(0, b1_cont,1):
+            if (b1_ctr[i, 0] - x_b1) ** 2 + (b1_ctr[i, 1] - y_b1) ** 2 + (b1_ctr[i, 2] - z_b1) ** 2 > (b1_ctr[i, 3] + r_b1) ** 2 + min_gap:
                 b1_avlb = b1_avlb * 1
             else:
                 b1_avlb = b1_avlb * 0
         if b1_avlb == 1:
-            b1_ctr[b1_cont - 1, 0] = x_b1
-            b1_ctr[b1_cont - 1, 1] = y_b1
-            b1_ctr[b1_cont - 1, 2] = z_b1
-            b1_ctr[b1_cont - 1, 3] = r_b1
+            b1_ctr[b1_cont, 0] = x_b1
+            b1_ctr[b1_cont, 1] = y_b1
+            b1_ctr[b1_cont, 2] = z_b1
+            b1_ctr[b1_cont, 3] = r_b1
             b1_cont += 1
         else:
             b1_cont = b1_cont
+
+# Location for aggregate type 2
+def b2_ctrs():
+    b2_cont = 0  # aggregate counter
+    while b2_cont < b2_num:
+        b2_avlb = 1
+        r_b2 = random.uniform(0.003e3, 0.004e3)
+        x_ = r_disk - r_b2 - min_gap
+        x_b2 = random.uniform(-x_, x_)
+        y_b2 = random.uniform(-math.sqrt(x_ ** 2 - x_b2 ** 2), math.sqrt(x_ ** 2 - x_b2 ** 2))
+        z_ = 2 * r_disk * td_ratio - 2 * r_b2 - 2 * min_gap
+        z_b2 = random.uniform(-z_ / 2, z_ / 2)
+        for i in range(0, int(f_num * (n * p + 1)),1):
+            if (sph_ctr[i, 0] - x_b2) ** 2 + (sph_ctr[i, 1] - y_b2) ** 2 + (sph_ctr[i, 2] - z_b2) ** 2 > (r_sph + r_b2) ** 2 + min_gap:
+                b2_avlb = b2_avlb * 1
+            else:
+                b2_avlb = b2_avlb * 0
+        for i in range(0, b1_num,1):
+            if (b1_ctr[i, 0] - x_b2) ** 2 + (b1_ctr[i, 1] - y_b2) ** 2 + (b1_ctr[i, 2] - z_b2) ** 2 > (b1_ctr[i, 3] + r_b2) ** 2 + min_gap:
+                b2_avlb = b2_avlb * 1
+            else:
+                b2_avlb = b2_avlb * 0
+        for i in range(0, b2_cont,1):
+            if (b2_ctr[i, 0] - x_b2) ** 2 + (b2_ctr[i, 1] - y_b2) ** 2 + (b2_ctr[i, 2] - z_b2) ** 2 > (b2_ctr[i, 3] + r_b2) ** 2 + min_gap:
+                b2_avlb = b2_avlb * 1
+            else:
+                b2_avlb = b2_avlb * 0
+        if b2_avlb == 1:
+            b2_ctr[b2_cont, 0] = x_b2
+            b2_ctr[b2_cont, 1] = y_b2
+            b2_ctr[b2_cont, 2] = z_b2
+            b2_ctr[b2_cont, 3] = r_b2
+            b2_cont += 1
+        else:
+            b2_cont = b2_cont
+
+# Location for aggregate type 3
+def b3_ctrs():
+    b3_cont = 0  # aggregate counter
+    while b3_cont < b3_num:
+        b3_avlb = 1
+        r_b3 = random.uniform(0.002e3, 0.003e3)
+        x_ = r_disk - r_b3 - min_gap
+        x_b3 = random.uniform(-x_, x_)
+        y_b3 = random.uniform(-math.sqrt(x_ ** 2 - x_b3 ** 2), math.sqrt(x_ ** 2 - x_b3 ** 2))
+        z_ = 2 * r_disk * td_ratio - 2 * r_b3 - 2 * min_gap
+        z_b3 = random.uniform(-z_ / 2, z_ / 2)
+        for i in range(0, int(f_num * (n * p + 1)),1):
+            if (sph_ctr[i, 0] - x_b3) ** 2 + (sph_ctr[i, 1] - y_b3) ** 2 + (sph_ctr[i, 2] - z_b3) ** 2 > (r_sph + r_b3) ** 2 + min_gap:
+                b3_avlb = b3_avlb * 1
+            else:
+                b3_avlb = b3_avlb * 0
+        for i in range(0, b1_num,1):
+            if (b1_ctr[i, 0] - x_b3) ** 2 + (b1_ctr[i, 1] - y_b3) ** 2 + (b1_ctr[i, 2] - z_b3) ** 2 > (b1_ctr[i, 3] + r_b3) ** 2 + min_gap:
+                b3_avlb = b3_avlb * 1
+            else:
+                b3_avlb = b3_avlb * 0
+        for i in range(0, b2_num,1):
+            if (b2_ctr[i, 0] - x_b3) ** 2 + (b2_ctr[i, 1] - y_b3) ** 2 + (b2_ctr[i, 2] - z_b3) ** 2 > (b2_ctr[i, 3] + r_b3) ** 2 + min_gap:
+                b3_avlb = b3_avlb * 1
+            else:
+                b3_avlb = b3_avlb * 0
+        for i in range(0, b3_cont,1):
+            if (b3_ctr[i, 0] - x_b3) ** 2 + (b3_ctr[i, 1] - y_b3) ** 2 + (b3_ctr[i, 2] - z_b3) ** 2 > (b3_ctr[i, 3] + r_b3) ** 2 + min_gap:
+                b3_avlb = b3_avlb * 1
+            else:
+                b3_avlb = b3_avlb * 0
+        if b3_avlb == 1:
+            b3_ctr[b3_cont, 0] = x_b3
+            b3_ctr[b3_cont, 1] = y_b3
+            b3_ctr[b3_cont, 2] = z_b3
+            b3_ctr[b3_cont, 3] = r_b3
+            b3_cont += 1
+        else:
+            b3_cont = b3_cont
+
+# Location for aggregate type 4
+def b4_ctrs():
+    b4_cont = 0  # aggregate counter
+    while b4_cont < b4_num:
+        b4_avlb = 1
+        r_b4 = random.uniform(0.001e3, 0.002e3)
+        x_ = r_disk - r_b4 - min_gap
+        x_b4 = random.uniform(-x_, x_)
+        y_b4 = random.uniform(-math.sqrt(x_ ** 2 - x_b4 ** 2), math.sqrt(x_ ** 2 - x_b4 ** 2))
+        z_ = 2 * r_disk * td_ratio - 2 * r_b4 - 2 * min_gap
+        z_b4 = random.uniform(-z_ / 2, z_ / 2)
+        for i in range(0, int(f_num * (n * p + 1)),1):
+            if (sph_ctr[i, 0] - x_b4) ** 2 + (sph_ctr[i, 1] - y_b4) ** 2 + (sph_ctr[i, 2] - z_b4) ** 2 > (r_sph + r_b4) ** 2 + min_gap:
+                b4_avlb = b4_avlb * 1
+            else:
+                b4_avlb = b4_avlb * 0
+        for i in range(0, b1_num):
+            if (b1_ctr[i, 0] - x_b4) ** 2 + (b1_ctr[i, 1] - y_b4) ** 2 + (b1_ctr[i, 2] - z_b4) ** 2 > (b1_ctr[i, 3] + r_b4) ** 2 + min_gap:
+                b4_avlb = b4_avlb * 1
+            else:
+                b4_avlb = b4_avlb * 0
+        for i in range(0, b2_num,1):
+            if (b2_ctr[i, 0] - x_b4) ** 2 + (b2_ctr[i, 1] - y_b4) ** 2 + (b2_ctr[i, 2] - z_b4) ** 2 > (b2_ctr[i, 3] + r_b4) ** 2 + min_gap:
+                b4_avlb = b4_avlb * 1
+            else:
+                b4_avlb = b4_avlb * 0
+        for i in range(0, b3_num,1):
+            if (b3_ctr[i, 0] - x_b4) ** 2 + (b3_ctr[i, 1] - y_b4) ** 2 + (b3_ctr[i, 2] - z_b4) ** 2 > (b3_ctr[i, 3] + r_b4) ** 2 + min_gap:
+                b4_avlb = b4_avlb * 1
+            else:
+                b4_avlb = b4_avlb * 0
+        for i in range(0, b4_cont,1):
+            if (b4_ctr[i, 0] - x_b4) ** 2 + (b4_ctr[i, 1] - y_b4) ** 2 + (b4_ctr[i, 2] - z_b4) ** 2 > (b4_ctr[i, 3] + r_b4) ** 2 + min_gap:
+                b4_avlb = b4_avlb * 1
+            else:
+                b4_avlb = b4_avlb * 0
+        if b4_avlb == 1:
+            b4_ctr[b4_cont, 0] = x_b4
+            b4_ctr[b4_cont, 1] = y_b4
+            b4_ctr[b4_cont, 2] = z_b4
+            b4_ctr[b4_cont, 3] = r_b4
+            b4_cont += 1
+        else:
+            b4_cont = b4_cont
 
 
 org_ctr = s_ctrs()
 f_ctrs()
 b1_ctrs()
+b2_ctrs()
+b3_ctrs()
+b4_ctrs()
 
 # Record fibre locations
-f1 = open('sph_ctr.txt', 'w')
+f1 = open('sph_ctrs.txt', 'w')
 for i in range(len(sph_ctr)):
     f1.write('%11.5f' % sph_ctr[i][0] + ',' + '%11.5f' % sph_ctr[i][1] + ',' + '%11.5f' % sph_ctr[i][2] + '\n')
 f1.close()
@@ -141,12 +270,25 @@ f3 = open('CS_rotat.txt', 'w')
 for i in range(len(local_cs)):
     f3.write('%11.5f' % local_cs[i][3] + ',' + '%11.5f' % local_cs[i][4] + ',' + '%11.5f' % local_cs[i][5] + '\n')
 f3.close()
-
-
-# Record B1 location
+# Record b1 location
 f1 = open('b1_ctrs.txt','w')
 for i in range(0, len(b1_ctr), 1):
    f1.write('%11.5f' % b1_ctr[i,0] +' '+'%11.5f' % b1_ctr[i,1]+' '+'%11.5f' % b1_ctr[i,2] +'%11.5f' % b1_ctr[i,3] +'\n')
+f1.close()
+# Record b2 location
+f1 = open('b2_ctrs.txt','w')
+for i in range(0, len(b2_ctr), 1):
+   f1.write('%11.5f' % b2_ctr[i,0] +' '+'%11.5f' % b2_ctr[i,1]+' '+'%11.5f' % b2_ctr[i,2] +'%11.5f' % b2_ctr[i,3] +'\n')
+f1.close()
+# Record b3 location
+f1 = open('b3_ctrs.txt','w')
+for i in range(0, len(b3_ctr), 1):
+   f1.write('%11.5f' % b3_ctr[i,0] +' '+'%11.5f' % b3_ctr[i,1]+' '+'%11.5f' % b3_ctr[i,2] +'%11.5f' % b3_ctr[i,3] +'\n')
+f1.close()
+# Record b4 location
+f1 = open('b4_ctrs.txt','w')
+for i in range(0, len(b4_ctr), 1):
+   f1.write('%11.5f' % b4_ctr[i,0] +' '+'%11.5f' % b4_ctr[i,1]+' '+'%11.5f' % b4_ctr[i,2] +'%11.5f' % b4_ctr[i,3] +'\n')
 f1.close()
 
 # plot spheres in matplotlib
@@ -164,7 +306,13 @@ def plt_all():
         for j in range(i * len(org_ctr), (i + 1) * len(org_ctr), 1):
             ax.scatter(sph_ctr[j][0], sph_ctr[j][1], sph_ctr[j][2], c=sphere_color[i])  # draw spheres (scatters)
     for i in range(0, len(b1_ctr), 1):
-        ax.scatter(b1_ctr[i][0], b1_ctr[i][1], b1_ctr[i][2], c='r')  # plot aggregate 1 (scatters)
+        ax.scatter(b1_ctr[i][0], b1_ctr[i][1], b1_ctr[i][2], c='b')  # plot aggregate 1 (scatters)
+    for i in range(0, len(b2_ctr), 1):
+        ax.scatter(b2_ctr[i][0], b2_ctr[i][1], b2_ctr[i][2], c='g')  # plot aggregate 2 (scatters)
+    for i in range(0, len(b3_ctr), 1):
+        ax.scatter(b3_ctr[i][0], b3_ctr[i][1], b3_ctr[i][2], c='r')  # plot aggregate 3 (scatters)
+    for i in range(0, len(b4_ctr), 1):
+        ax.scatter(b4_ctr[i][0], b4_ctr[i][1], b4_ctr[i][2], c='c')  # plot aggregate 3 (scatters)
     plt.show()
 
 
